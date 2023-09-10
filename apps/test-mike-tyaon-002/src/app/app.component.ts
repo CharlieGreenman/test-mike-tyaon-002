d65917fc659e4649234dca03a994430e4d7a9464
Charlie Greenman <charlie@razroo.com>
2023-09-10 16:08:59 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/test-mike-tyaon-002/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-mike-tyaon-002-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-mike-tyaon-002';
+}
